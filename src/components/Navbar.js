3c3da1eaa7dda60498823460fc30ab00bfc20ddc
Mayuresh <mj.mayureshjagtap@gmail.com>
2021-11-14 11:01:28 +1100

    database Updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,9 @@ import React from 'react'
 
 export const Navbar = () => {
 
-<<<<<<< HEAD
-export const Navbar = ({ user }) => {
 
-
-    return (
-=======
 return (
->>>>>>> 6ee193a7c3d7730322b34ca6fa53d26277f4c6e2
+
         <div className='navbox'>
             Navbar
         </div>
--- src/config/Config.js
+++ src/config/Config.js
@@ -6,12 +6,13 @@ import 'firebase/auth'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB53rrQAOrnkRSoaylDOMOaAKRRcdYkbxA",
-  authDomain: "auth-development-9284a.firebaseapp.com",
-  projectId: "auth-development-9284a",
-  storageBucket: "auth-development-9284a.appspot.com",
-  messagingSenderId: "443773910752",
-  appId: "1:443773910752:web:06eeaf39f446517b08da85"
+  apiKey: "AIzaSyCfVyx4n9jacY3E5vw-DJeekO3UItIs2JA",
+  authDomain: "wear-this-f2eca.firebaseapp.com",
+  projectId: "wear-this-f2eca",
+  storageBucket: "wear-this-f2eca.appspot.com",
+  messagingSenderId: "829399538409",
+  appId: "1:829399538409:web:af3dba3584056b4b2dae78",
+  measurementId: "G-LM2DQ2SKR9"
 };
 
 firebase.initializeApp(firebaseConfig);
