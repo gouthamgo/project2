bee6144605a8d9759fcf12eb98e0e82404a90337
Goutham <goutham@Goutham-2.local>
2021-11-15 19:27:39 +1100

    nav fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,39 @@ export const Navbar = () => {
 
 
 return (
-        <div className='navbox'>
-          <Link to='signup' className='nav-link'>Signup</Link>
-            <Link to='login' className='nav-link'>Login</Link>
-        </div>
+  <>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <a class="navbar-brand" href="/">Wear<span> This</span></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse " id="navbarNav" >
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item active">
+            <Link to= '/' className="nav-link"> Home</Link>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#ban">New</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+
+          <li class="nav-item">
+            <Link to= '/signup' className="nav-link"> Signup</Link>
+          </li>
+
+          <li class="nav-item">
+            <Link to= '/login' className="nav-link"> Login</Link>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
+
+  </>
     )
 }
