baba0232cdff17145fcd6c5f94125ad28d6d9be5
Mayuresh <mj.mayureshjagtap@gmail.com>
2021-11-15 18:19:24 +1100

    Auth

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React, {Component} from 'react'
-import { Home } from './components/Home'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import {AddProducts} from './components/AddProducts'
+import React, { Component } from 'react';
+import { Home } from './components/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { AddProducts } from './components/AddProducts';
 import { Signup } from './components/Signup';
 import { Login } from './components/Login';
-import {Women} from './components/Women'
+import { Women } from './components/Women';
+import { auth, db } from './config/Config';
+
 
 
 export class App extends Component {
+
+  state = {
+    user: null
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        db.collection('Users').doc(user.uid).get().then(snapshot => {
+          this.setState({
+            user: snapshot.data().Name
+          })
+        })
+      }
+      else {
+        this.setState({
+          user: null
+        })
+      }
+    })
+  }
+
   render(){
     return(
       <div>
         <BrowserRouter>
           <Switch>
 
-            <Route exact path='/' component={Home} />
+            <Route exact path='/' component={() => <Home user={this.state.user} />} />
             {/* Home componentgonna be rendered for the user interaction */}
 
             <Route exact path='/addproducts' component={AddProducts} />
